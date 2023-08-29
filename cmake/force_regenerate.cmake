4ece2fc093cbaafe8d247cf54ab4db1e3fbfc4fe
Thibault Poignonec <79221188+tpoignonec@users.noreply.github.com>
2023-08-29 07:12:40 -0500

    Fix cmake regeneration (#2)
    * use 'custom_target' to force a regeneration
    
    * export findPkg cmake files for libs used by Acados
    
    * clean-up and update README.md

---
+++ cmake/force_regenerate.cmake
@@ -0,0 +1,23 @@
+# Copyright 2023 ICUBE Laboratory, University of Strasbourg
+# License: Apache License, Version 2.0
+# Author: Thibault Poignonec (tpoignonec@unistra.fr)
+
+# Part of the 'acados_vendor_ros2' package
+
+function(force_regenerate_at_next_build)
+    set( FLAG_FILE_REBUILD ${CMAKE_CURRENT_SOURCE_DIR}/.flag_regenerate_cmake )
+    execute_process( COMMAND touch ${FLAG_FILE_REBUILD} )
+    add_custom_target(regenerate_cmake ALL)
+    add_custom_command(
+        TARGET  regenerate_cmake
+        DEPENDS ${FLAG_FILE_REBUILD}
+        COMMAND rm -f ${FLAG_FILE_REBUILD}
+        COMMENT "The cmake configuration will be regenerated at next build."
+    )
+    set_property(
+        DIRECTORY
+        APPEND
+        PROPERTY CMAKE_CONFIGURE_DEPENDS
+        ${FLAG_FILE_REBUILD}
+    )
+endfunction()
